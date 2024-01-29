987b8eb40c171d78f94b1f1aed665f18a60d84ee
Brad Sherman <bsherman1096@gmail.com>
2024-01-29 11:02:51 -0600

    Remove OmitNothingFields test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,17 +5,16 @@ module Main where
 import Test.Hspec
 
 import qualified Basic
+import qualified ClosedTypeFamilies
 import qualified Formatting
 import qualified Generic
 import qualified GetDoc
 import qualified HigherKind
-import qualified ClosedTypeFamilies
 
 import qualified LegalNameSpec
 import qualified NoOmitNothingFields
 import qualified ObjectWithSingleFieldNoTagSingleConstructors
 import qualified ObjectWithSingleFieldTagSingleConstructors
-import qualified OmitNothingFields
 import qualified TaggedObjectNoTagSingleConstructors
 import qualified TaggedObjectTagSingleConstructors
 import qualified TwoElemArrayNoTagSingleConstructors
@@ -24,27 +23,24 @@ import qualified UntaggedNoTagSingleConstructors
 import qualified UntaggedTagSingleConstructors
 import qualified UnwrapUnaryRecords
 
-
 main :: IO ()
 main = hspec $ parallel $ do
-  Basic.tests
-  ClosedTypeFamilies.tests
-  Formatting.tests
-  Generic.tests
+    Basic.tests
+    ClosedTypeFamilies.tests
+    Formatting.tests
+    Generic.tests
+    HigherKind.tests
+    LegalNameSpec.tests
+    NoOmitNothingFields.allTests
+    ObjectWithSingleFieldNoTagSingleConstructors.tests
+    ObjectWithSingleFieldTagSingleConstructors.tests
+    TaggedObjectNoTagSingleConstructors.tests
+    TaggedObjectTagSingleConstructors.tests
+    TwoElemArrayNoTagSingleConstructors.tests
+    TwoElemArrayTagSingleConstructors.tests
+    UntaggedNoTagSingleConstructors.tests
+    UntaggedTagSingleConstructors.tests
+    UnwrapUnaryRecords.allTests
 #if MIN_VERSION_template_haskell(2,18,0)
-  GetDoc.tests
+    GetDoc.tests
 #endif
-  HigherKind.tests
-
-  LegalNameSpec.tests
-  NoOmitNothingFields.allTests
-  ObjectWithSingleFieldNoTagSingleConstructors.tests
-  ObjectWithSingleFieldTagSingleConstructors.tests
-  OmitNothingFields.tests
-  TaggedObjectNoTagSingleConstructors.tests
-  TaggedObjectTagSingleConstructors.tests
-  TwoElemArrayNoTagSingleConstructors.tests
-  TwoElemArrayTagSingleConstructors.tests
-  UntaggedNoTagSingleConstructors.tests
-  UntaggedTagSingleConstructors.tests
-  UnwrapUnaryRecords.allTests
